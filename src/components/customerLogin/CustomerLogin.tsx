388fcc2d31ea6a792a6d9b70f5ef493e50756f2c
krt <52488299+ShlokRamteke@users.noreply.github.com>
2023-06-26 18:42:07 +0530

    CustomerLogin

--- src/components/customerLogin/CustomerLogin.tsx
+++ src/components/customerLogin/CustomerLogin.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 
 import styles from './CustomerLogin.module.css'
+import Button from "../UI/Button";
 
 const CustomerLogin: React.FC<any> =props => {
     return < React.Fragment>
     <div className={styles.centeredContainer}>
         <div className={styles.customerLoginCard}>
-                <div>
-                    <input type="text" placeholder="Enter your Username" />
-                </div>
+            <h3 className={styles.customerLoginTitle}>Customer SignIn</h3>
+            <input type="text" className={styles.input} placeholder="Enter your Username" />
+            <input type="password" className={styles.input} placeholder="Enter your Password" />
+            <button className={styles.loginButton}>Log In</button>
             </div>
     </div>
         
